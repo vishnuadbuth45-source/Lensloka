299d82fb6b4d6214bf67f3a93996dbea4813030d
vishnu <vishnu.adbuth45@gmail.com>
2026-08-19 17:49:56 +0530

    fixed some build errors

--- app/creators/page.tsx
+++ app/creators/page.tsx
@@ -1,8 +1,13 @@
 import { CreatorsContent } from "./creators-content"
 import { getCreators } from "@/lib/queries/creators"
+import { Suspense } from "react"
 
 export default async function CreatorsPage() {
   const creators = await getCreators()
 
-  return <CreatorsContent creators={creators} />
+  return (
+    <Suspense fallback="Loading creators...">
+      <CreatorsContent creators={creators} />
+    </Suspense>
+  )
 }
